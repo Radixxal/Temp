ac1d2ebde02766e48cf9357e38a05998783456a3
Radixxal <98231034+Radixxal@users.noreply.github.com>
2022-11-15 13:05:56 +0100

    Add files via upload

---
+++ lab03-var07.sql
@@ -0,0 +1,91 @@
+
+-- actors
+INSERT INTO actors(name, role) VALUES('Kenny Rose', 'Jimmy'),
+('Ethan Carter', 'Son of President'),
+('Derek Rose', 'Derek Rose'),
+('Joan Smit', 'Special Agent 008'),
+('Naomi Butch', 'Emily'),
+('Stella Bogomi', 'F-98'),
+('Jack Masaki', 'Sherif'),
+('Andrew Johrood', 'Tupac Shakur'),
+('Boris Fidger', 'Eminem'),
+('Malcolm Alabown', 'Barak Obama'),
+('Carl Jigly', 'Tommy'),
+('Chad Rose', 'Father'),
+('Maksim Altufiev', 'Thief'),
+('Eva Rosenberg', 'Mobly'),
+('Anthony Braus', 'Mark Sanly'),
+('Anderson Grim', 'John Diglan'),
+('George Lichy', 'Master 5Gen'),
+('Travis Shinly', 'Pilot of MassacroSh-0234'),
+('Nikita Potov', 'Elliot Boston'),
+('Wayne Eall', 'Maxim Brechev');
+
+-- festivals
+INSERT INTO festivals(title_fest, year_fest) VALUES
+('Nora Fest Hamburg 2020', 2020),
+('Moscow FutureKino Awards 2022', 2022),
+('France Fest 2021', 2021),
+('UnderBeach Movie Fest 2019', 2019),
+('Mountain Movie: best debut', 2019),
+('Moscow FutureKino Awards 2023', 2023),
+('Nora Fest Hamburg 2013', 2013),
+('Nora Fest Hamburg 2014', 2014),
+('Nora Fest Hamburg 2015', 2015),
+('Nora Fest Hamburg 2016', 2016),
+('Nora Fest Hamburg 2017', 2017),
+('Moscow FutureKino Awards 2000', 2000),
+('Moscow FutureKino Awards 2002', 2002),
+('Moscow FutureKino Awards 2001', 2001),
+('Wrong movie by NY Times 2022', 2022),
+('Nord Fest: Europe Movies', 2010),
+('Wrong movie by NY Times 2020', 2020),
+('SpainMovie 2009', 2009),
+('BrazilFest Rio 2014', 2014),
+('HighFest Baltic 2000', 2000),
+('First MovieFest 2024', 2024);
+
+-- atributes
+insert into atributes (author, director, main_role, award) values ('Gayleen Davioud', 'Theodore Blackford', 16, 20);
+insert into atributes (author, director, main_role, award) values ('Pat Fosher', 'Masha Crozier', 18, 19);
+insert into atributes (author, director, main_role, award) values ('Karyn Covely', 'Lita Lettington', 20, 2);
+insert into atributes (author, director, main_role, award) values ('Nissie Harbertson', 'Wendell Reilinger', 12, 18);
+insert into atributes (author, director, main_role, award) values ('Zonda Falkous', 'Caitlin Baise', 17, 17);
+insert into atributes (author, director, main_role, award) values ('Elmira Harm', 'Martina Giddens', 8, 6);
+insert into atributes (author, director, main_role, award) values ('Aeriell Buckley', 'Vincenty Pettiford', 1, 16);
+insert into atributes (author, director, main_role, award) values ('Lyndsie Westbury', 'Cordelia Haddrill', 4, 15);
+insert into atributes (author, director, main_role, award) values ('Germain Sharrier', 'Elinor OIlier', 11, 14);
+insert into atributes (author, director, main_role, award) values ('Ruddy Bill', 'Caritta Ramstead', 3, 13);
+insert into atributes (author, director, main_role, award) values ('Zoe Grosvenor', 'Lindy Speake', 2, 12);
+insert into atributes (author, director, main_role, award) values ('Correy Drews', 'Cacilia Darragh', 10, 11);
+insert into atributes (author, director, main_role, award) values ('Hermy Grinvalds', 'Amalee Heinert', 5, 8);
+insert into atributes (author, director, main_role, award) values ('Roderich Harrowsmith', 'Chloris Lanney', 20, 10);
+insert into atributes (author, director, main_role, award) values ('Chevy Joselovitch', 'Lynnell Plevey', 6, 9);
+insert into atributes (author, director, main_role, award) values ('Griffith Parades', 'Clarabelle Murrill', 7, 8);
+insert into atributes (author, director, main_role, award) values ('Carly Bortolomei', 'Donn Showering', 9, 7);
+insert into atributes (author, director, main_role, award) values ('Graham Gally', 'Ezra Mellem', 14, 6);
+insert into atributes (author, director, main_role, award) values ('Kerrin Worthy', 'Harmon Skoggins', 19, 5);
+insert into atributes (author, director, main_role, award) values ('Susann Mabbett', 'Concordia Maypother', 15, 4);
+insert into atributes (author, director, main_role, award) values ('Percival Girone', 'Gilberte Macenzy', 21, 3);
+
+-- videotapes
+insert into videotapes (title, year, atributes) values ('Drum, The (Drums)', 1993, 19);
+insert into videotapes (title, year, atributes) values ('Titfield Thunderbolt, The', 2007, 9);
+insert into videotapes (title, year, atributes) values ('Second Best', 1988, 10);
+insert into videotapes (title, year, atributes) values ('Chattahoochee', 2004, 11);
+insert into videotapes (title, year, atributes) values ('Reservation Road', 1999, 14);
+insert into videotapes (title, year, atributes) values ('Home Alone 4', 1992, 13);
+insert into videotapes (title, year, atributes) values ('Fifth Commandment, The', 1995, 12);
+insert into videotapes (title, year, atributes) values ('Company of Heroes', 1999, 15);
+insert into videotapes (title, year, atributes) values ('Mumia Abu-Jamal: A Case for Reasonable Doubt?', 2013, 1);
+insert into videotapes (title, year, atributes) values ('Half of a Yellow Sun', 2019, 20);
+insert into videotapes (title, year, atributes) values ('5th Musketeer, The (a.k.a. Fifth Musketeer, The)', 1990, 8);
+insert into videotapes (title, year, atributes) values ('Oppressed Majority', 2022, 3);
+insert into videotapes (title, year, atributes) values ('I Am Ali', 2003, 5);
+insert into videotapes (title, year, atributes) values ('Ruby Gentry', 2018, 6);
+insert into videotapes (title, year, atributes) values ('Case Against 8, The', 1985, 2);
+insert into videotapes (title, year, atributes) values ('Mahjong (Ma jiang)', 2009, 4);
+insert into videotapes (title, year, atributes) values ('Man with the Iron Fists, The', 1981, 7);
+insert into videotapes (title, year, atributes) values ('Tall in the Saddle', 1991, 16);
+insert into videotapes (title, year, atributes) values ('Halo Legends', 1996, 17);
+insert into videotapes (title, year, atributes) values ('Western Union', 1982, 18);
